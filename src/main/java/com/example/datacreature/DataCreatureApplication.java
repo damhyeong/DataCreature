f00aba5d6e6135a8da65e95bc9c8508df289829f
damhyeong <rhdwhdals8@naver.com>
2023-11-14 19:46:52 +0900

    first commit

---
+++ src/main/java/com/example/datacreature/DataCreatureApplication.java
@@ -0,0 +1,13 @@
+package com.example.datacreature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataCreatureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataCreatureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datacreature/DataCreatureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datacreature;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataCreatureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
