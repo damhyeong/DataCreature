81c708f42206469ce4d2fcef26e2cd3fad0f4807
damhyeong <rhdwhdals8@naver.com>
2023-11-22 20:20:08 +0900

    Complete Changing HttpSecurity return Method reason Old Pattern Does not Work. At 23.11.22-8PM

--- src/main/java/com/example/datacreature/config/WebSecurityConfig.java
+++ src/main/java/com/example/datacreature/config/WebSecurityConfig.java
@@ -4,10 +4,14 @@ import com.example.datacreature.filter.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configurable
 @EnableWebSecurity
@@ -17,16 +21,35 @@ public class WebSecurityConfig {
 
     @Bean
     protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+        /*
         httpSecurity
                 .cors().and()
                 .csrf().disable()
                 .httpBasic().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests()
-                .antMatchers()
-                .antMatchers
+                //.antMatchers().permitAll()
+                .requestMatchers(HttpMethod.GET, "/api/v1/board").permitAll()
+                .requestMatchers("/", "/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated();
 
+                httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                return httpSecurity.build();
+         */
+        httpSecurity
+                .cors(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .sessionManagement(
+                        sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(request -> {
+                        request.requestMatchers(HttpMethod.GET, "/api/v1/board", "/api/v1/user/*").permitAll();
+                        request.requestMatchers("/", "/api/v1/auth/**", "/api/v1/search/**", "/file/**").permitAll();
+                        request.anyRequest().authenticated();
+                })
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-
+        return httpSecurity.build();
     }
 }
